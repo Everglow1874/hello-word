62ebe49c4d17f22bcc38d6bdce612b5eb88584fb
Everglow1874 <everglow1874@163.com>
2024-10-10 21:01:38 +0800

    test

---
+++ src/main/java/com/zhan/code/HelloWord.java
@@ -0,0 +1,14 @@
+package com.zhan.code;
+
+/**
+ * @Classname HelloWord
+ * @Description TODO
+ * @Date 2024-10-10 20:59
+ * @Created by wujiazhan
+ */
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
