8c0ca592d6be0ab3a68d82eaba0b316447622248
Everglow1874 <everglow1874@163.com>
2024-10-10 21:06:48 +0800

    修改测试

--- src/main/java/com/zhan/code/HelloWord.java
+++ src/main/java/com/zhan/code/HelloWord.java
@@ -10,5 +10,10 @@ package com.zhan.code;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
+
+
+        System.out.println("dfhsadlf");
+
+
     }
 }
